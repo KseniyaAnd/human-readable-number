336e7fba361d47548fe792e2e6ca1ad7197a96ab
KseniyaAnd <kseniya.andrusenko72@gmail.com>
2023-09-06 22:19:30 +0300

    init: human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let result = '';
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (number >= 1000) {
+        result += ones[Math.floor(number / 1000) - 1] + ' thousand ';
+        number %= 1000;
+    }
+
+    if (number >= 100) {
+        result += ones[Math.floor(number / 100) - 1] + ' hundred ';
+        number %= 100;
+    }
+
+    if (number >= 20) {
+        result += tens[Math.floor(number / 10) - 1] + ' ';
+        number %= 10;
+    } else if (number >= 11) {
+        result += teens[number - 11] + ' ';
+        number = 0;
+    }
+
+    if (number > 0) {
+        result += ones[number - 1];
+    }
+
+    return result.trim();
 }
